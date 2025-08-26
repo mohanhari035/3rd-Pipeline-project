d395e1c42e3e83a1d37a90aab1c6da760398ce7d
mohanhari035 <harimohan.mh@gmail.com>
2025-08-27 01:54:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "resource-abhimanyu" {
 resource "azurerm_storage_account" "example" {
   for_each = var.storageaccountname
   name                     = each.key
-  resource_group_name      = azurerm_resource_group.resource-abhimanyu[each.value].name
+  resource_group_name      = azurerm_resource_group.resource-abhimanyu[each.key].name
   location                 = each.value.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
